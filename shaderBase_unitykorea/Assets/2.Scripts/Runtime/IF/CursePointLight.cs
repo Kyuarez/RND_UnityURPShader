3866a11df207ee35bd21686d5e9717be704cda36
htk0575 <htk008kr@naver.com>
2025-08-28 18:42:08 +0900

    [feat] 쉐이더 그래프 공부
    1. Disolve #uv - split + noise
    2. lava - noise + gradient
    3. lighting - noise + noise + remap + rectangle
    4. pointlight - lightposition

---
+++ shaderBase_unitykorea/Assets/2.Scripts/Runtime/IF/CursePointLight.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace IF
+{
+    /// <summary>
+    /// IF: 포인트 라이트의 위치 값을 쉐이더 변수에 업데이팅
+    /// </summary>
+    [RequireComponent(typeof(MeshRenderer))]
+    public class CursePointLight : MonoBehaviour
+    {
+        [SerializeField] Transform m_PointLightTrs;
+        
+        Material m_CurseMaterial;
+
+        private void Awake()
+        {
+            m_CurseMaterial = GetComponent<MeshRenderer>().material;
+        }
+
+        private void Update()
+        {
+            m_CurseMaterial.SetVector("_LightPosition", m_PointLightTrs.position);
+        }
+    }
+
+}
+
